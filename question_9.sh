785c6015f60c268c738e7691da959a655b319d2d
patrickrpandrade <108357106+patrickrpandrade@users.noreply.github.com>
2023-01-02 17:55:52 -0300

    Update question_9.sh

--- question_9.sh
+++ question_9.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+input_file="$1"
+output_file="/tmp/output_1.txt"
+echo "" > ${output_file}
+while IFS= read -r line
+do
+  if [[ "$line" = "\#*" ]]
+  then
+    echo "aqui"
+    echo $line >> ${output_file}
+  else
+    fake_ip=`echo $line | cut -d" " -f1`
+    echo $fake_ip
+    repeated=`grep -ci $fake_ip ${output_file}`
+    echo $repeated
+    if [[ $repeated -lt 1 ]]
+    then
+      echo "acola"
+      echo $line >> ${output_file}
+    fi
+  fi
+done < ${input_file}
